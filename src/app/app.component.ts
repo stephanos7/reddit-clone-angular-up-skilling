94aab053cca0d204b9c116827ed9299f1a777dc4
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-08-27 19:30:51 +0200

    add dynamic article creation and rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from "./components/article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles : Article[];
 
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
-    console.log(`Adding a title: ${title.value} with a link: ${link.value}`);
-    return false;
+  constructor() { this.articles = [
+    new Article('Angular 2', 'http://angular.io', 3),
+    new Article('Fullstack', 'http://fullstack.io', 2),
+    new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`); 
+    this.articles.push(new Article(title.value, link.value, 0)); //create a new Article instance with the submitted title and URL as per the model-class contract. Add it to the array of Articles and
+    title.value = '';   // clear the input field value for "title"
+    link.value = '';    // clear the input field value for "link"
+    return false;       // prevent page load-refresh
   }
 }
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from './article.model'
 
 @Component({
@@ -8,14 +8,11 @@ import { Article } from './article.model'
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = "row";
-  article: Article;
+  @Input() article: Article;
 
-  constructor() { 
-    this.article = new Article("Angular 2", "http://angular.io", 0); //parameters expected as defined in model class
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   voteUp():boolean{
     this.article.voteUp(); // access the model-class functions
