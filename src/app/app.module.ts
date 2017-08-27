dc9f1561b408a8bd7fdaab4326f5f0e267fb9e66
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-08-27 17:12:26 +0200

    add component-html to article component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
